c9ab392e87be1da7a1beaccca45eaedb2a0c4c7f
liufeng <you@example.com>
2022-05-25 22:53:25 +0800

    OpenAPI Swagger 使用

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,12 +1,13 @@
 package server
 
 import (
-	v1 "helloworld/api/helloworld/v1"
-	"helloworld/internal/conf"
-	"helloworld/internal/service"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/swagger-api/openapiv2"
+	v1 "helloworld/api/helloworld/v1"
+	"helloworld/internal/conf"
+	"helloworld/internal/service"
 )
 
 // NewHTTPServer new a HTTP server.
@@ -27,5 +28,9 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 	}
 	srv := http.NewServer(opts...)
 	v1.RegisterGreeterHTTPServer(srv, greeter)
+
+	openAPIhandler := openapiv2.NewHandler()
+	srv.HandlePrefix("/q/", openAPIhandler)
+
 	return srv
 }
